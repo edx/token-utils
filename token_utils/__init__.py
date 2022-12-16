e52e00127e60e12dad71e50331865c7271aa86ec
Varsha Menon <varshamenon04@gmail.com>
2022-12-16 09:58:45 -0500

    build: fix build errors and warnings

--- token_utils/__init__.py
+++ token_utils/__init__.py
@@ -2,6 +2,6 @@
 App for the creation, signing, and decoding of various access tokens.
 """
 
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 
 default_app_config = 'token_utils.apps.TokenUtilsConfig'  # pylint: disable=invalid-name
